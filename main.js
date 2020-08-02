45cb3d711b875fbc2dc86e7f0b69c2852afe4b10
masa6412 <last_wwi@yahoo.co.jp>
2020-08-02 16:14:17 +0900

    コミット

---
+++ main.js
@@ -0,0 +1,52 @@
+// const form = document.getElementById('form')
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault()
+
+//   const price = document.getElementById('price').value
+//   const result = document.getElementById('result')
+
+//   if (document.getElementById('keigen').checked) {
+//     result.style.color = 'green'
+//     result.style.fontWeight = 'bold'
+//     result.textContent = `${Math.round(parseInt(price) * 0.08)}円`
+//   } else {
+//     result.style.color = 'black'
+//     result.style.fontWeight = 'normal'
+//     result.textContent = `${Math.round(parseInt(price) * 0.1)}円`
+//   }
+// })
+
+
+// const form = document.getElementById('form');
+// const result = document.getElementById('result');
+// const price = getElementById('price');
+
+// function calculateTax(price, isRelief) {
+//     if(document.getElementById('keigen').checked) {
+//         result.textContent = result.textContent(`${Math.round(parseInt) * 0.08}円`)
+//     }
+// }
+
+
+
+const items = [
+    // 日  月  火  水  木  金  土
+      [14, 93, 89, 51, 85, 59, 33],
+      [69, 27, 40, 76, 25, 21, 55],
+      [55, 60,  3, 28, 49,  5, 91],
+      [19, 56, 92, 66, 53, 80, 13],
+    ]
+    const sums = items.map((item) => {
+       let total = 0
+        for (i = 0; i <= item[0].length; i++) {
+          
+        }
+        return total += i
+    })
+    console.log(sums)
+    
+
+    item[a][b]
+    let a = item[x].length
+    let b = a[y]
